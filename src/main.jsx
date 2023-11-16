0b34e8883468ce544a7bb591d7360b385bcadaee
LucasAEscudero <lucasescudero5629@gmail.com>
2023-11-15 22:00:32 -0300

    reset URL

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import axios from 'axios'
 import './index.css'
 
 // axios.defaults.baseURL = 'http://localhost:3001'
-axios.defaults.baseURL = 'https://videogamesbackend-production-eedc.up.railway.app/';
+axios.defaults.baseURL = 'https://videogamesback-production-a383.up.railway.app/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
